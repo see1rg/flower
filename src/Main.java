7c4724f58df8aac441003fd57e07107976405731
Sergey Lyubimov <see1rg@mail.ru>
2022-10-02 21:17:25 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,31 @@
 
 public class Main {
     public static void main(String[] args) {
-        Flower rose = new Flower(null, "Россия", 35.595, 0);
+        Flower rose = new Flower(null, "Россия", 35.595, 4);
         Flower chrysanthemum = new Flower(null, null, 15, 5);
         Flower peony = new Flower(null, "England", 69.999, 1);
         Flower gypsophila = new Flower(null, "Турция", 19.5, 10);
-        //int makeBouquet(){
-//            double sum = (1 * rose.getCost() + 1 * chrysanthemum.getCost() + 1 * peony.getCost() + 1 * gypsophila.getCost());
-//            double sumDuty = sum+ sum*0.1;
+
+
+        printCostOfFlowers( rose,rose,rose,
+                chrysanthemum,chrysanthemum,
+                gypsophila );
 
         System.out.println(peony.toString());
+
+
 }
+
+    private static void printCostOfFlowers(Flower... flowers) {
+        int minimumLifeSpan = Integer.MAX_VALUE;
+        double totalCost = 0;
+        for (Flower flower : flowers) {
+           if(flower.getLifeSpan()< minimumLifeSpan){
+               minimumLifeSpan = flower.getLifeSpan(); }
+            totalCost += flower.getCost();
+        }
+        System.out.println("Стоимость букета: " + totalCost * 1.1);
+        System.out.println("Срок стояния букета: " + minimumLifeSpan);
+
+    }
 }
